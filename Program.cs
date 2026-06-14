b994fd5dc1063a56f02b9bb02a07a55cc03e596e
raahmok <tylusjohny09@gmail.com>
2026-06-14 18:39:09 +0200

    init sqlite

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sqlite!");
+            CreateConnection();
+        }
+
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+                Console.WriteLine("Connection worked!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection didn't work!");
+            }
+            return sqlite_conn;
+        }
+    }
+}
